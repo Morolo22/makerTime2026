3f6e9c021fb467a231f1cde6c3489c04642f8727
Morolo22 <morolo2223@gmail.com>
2026-06-04 18:53:28 +0300

    added searh and started work at time

--- script.js
+++ script.js
@@ -5,17 +5,41 @@ let weather
 let information = document.getElementById('information')
 let submit = document.getElementById('submit')
 
+let searchForm = document.getElementById('searchForm')
+
 
 submit.addEventListener('click', () =>{
+const whatDay = document.getElementById('whatDay')
+const whatHour = document.getElementById('whatHour')
 const city = document.getElementById('city')
-let url = `http://api.weatherapi.com/v1/current.json?key=980ca1ab957e49aa911142605262805&q=${city.value}&aqi=no`
-
+let url = `http://api.weatherapi.com/v1/current.json?key=980ca1ab957e49aa911142605262805&q=${city.value}&days=4&aqi=no&alerts=no`
+console.log(whatDay.value)
 
     fetch(url)
     .then(async function(response) {
         weather = await response.json()
         let current = weather.current
         let location = weather.location
+        console.log(location.localtime_epoch)
+        draw(current, location)
+    })
+})
+
+
+
+searchForm.addEventListener('submit', (e) =>{
+e.preventDefault()
+const whatDay = document.getElementById('whatDay')
+const city = document.getElementById('searchInp')
+let url = `http://api.weatherapi.com/v1/current.json?key=980ca1ab957e49aa911142605262805&q=${city.value}&days=4&aqi=no&alerts=no`
+console.log(whatDay.value)
+
+    fetch(url)
+    .then(async function(response) {
+        weather = await response.json()
+        let current = weather.forecast.forecastday.whatDay.hour.whatHour.condition
+        let location = weather.location
+        console.log(location.localtime_epoch)
         draw(current, location)
     })
 })
@@ -25,16 +49,25 @@ let url = `http://api.weatherapi.com/v1/current.json?key=980ca1ab957e49aa9111426
 
 
 function draw(current, location){
+    information.style.visibility = "visible";
     information.innerHTML = (`
     <h1>${location.name}</h1>
-
-    <h2>Температура</h2>
-    <h3 id="temp">${current.temp_c}°C</h3>
-    <h4 id="tempfeeling">відчувається як ${current.feelslike_c}°C</h4>
-    <h2>Погода</h2>
-    <img src="${current.condition.icon}" alt="">
-    <h3 id="weather">${current.condition.text}</h3>
-    <h3 id="windSpeed">Швидкість вітру ${current.wind_kph} км/год</h3>
+    <div class="getInfo">
+    <h2 class="getInfoBox" >Температура</h2>
+    <h3 class="getInfoBox" id="temp">${current.temp_c}°C</h3>
+    <h4 class="getInfoBox" id="tempfeeling">відчувається як ${current.feelslike_c}°C</h4>
+    <h2 class="getInfoBox">Погода</h2>
+    <img class="getInfoBox" src="${current.condition.icon}" alt="">
+    <h3 class="getInfoBox" id="weather">${current.condition.text}</h3>
+    </div>
+    <div class="getInfoWind">
+    <h3 class="getInfoBox" id="vis">Видимість ${current.vis_km} км</h3>
+    <h3 class="getInfoBox" id="windSpeed">Швидкість вітру ${current.wind_kph} км/год</h3>
+    <h3 class="getInfoBox" id="gustSpeed">Швидкість поривів ${current.gust_kph} км/год</h3>
+    <h3 class="getInfoBox" id="windDir">Напрям вітру ${current.wind_dir}</h3>
+    </div>
+    
     <h4>Час останього оновлення ${current.last_updated}</h4>
+    
         `)
 }

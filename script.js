74b1682bb6026963905cb01643ba1e24e0544e9b
Morolo22 <morolo2223@gmail.com>
2026-06-11 18:55:59 +0300

    added time and some css

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let weather
 
+let weatherTypes = ['Sunny','Clear','Partly Cloudy','Cloudy','Haze','Sandstorm','Mist','Patchy rain nearby','Patchy snow nearby','Blizzard','Fog','Light rain']
+let weatherColor = ['#ebd186','#43508f','#d4dbff','#ebeeff','#ffffff','#b5b87f','#8c89ab','#a39fc7','#dedee0','#7c7c80','#999999','#9d9ec7']
 
 
 let information = document.getElementById('information')
 let submit = document.getElementById('submit')
+let main = document.getElementById('main')
 
 let searchForm = document.getElementById('searchForm')
 
@@ -12,13 +15,15 @@ submit.addEventListener('click', () =>{
 const whatDay = document.getElementById('whatDay')
 const whatHour = document.getElementById('whatHour')
 const city = document.getElementById('city')
-let url = `http://api.weatherapi.com/v1/current.json?key=980ca1ab957e49aa911142605262805&q=${city.value}&days=4&aqi=no&alerts=no`
+let url = `http://api.weatherapi.com/v1/forecast.json?key=980ca1ab957e49aa911142605262805&q=${city.value}&days=4&aqi=no&alerts=no`
 console.log(whatDay.value)
 
     fetch(url)
     .then(async function(response) {
         weather = await response.json()
-        let current = weather.current
+        
+        
+        let current = weather.forecast.forecastday[whatDay.value].hour[whatHour.value]
         let location = weather.location
         console.log(location.localtime_epoch)
         draw(current, location)
@@ -31,13 +36,25 @@ searchForm.addEventListener('submit', (e) =>{
 e.preventDefault()
 const whatDay = document.getElementById('whatDay')
 const city = document.getElementById('searchInp')
-let url = `http://api.weatherapi.com/v1/current.json?key=980ca1ab957e49aa911142605262805&q=${city.value}&days=4&aqi=no&alerts=no`
+const whatHour = document.getElementById('whatHour')
+let url = `http://api.weatherapi.com/v1/forecast.json?key=980ca1ab957e49aa911142605262805&q=${city.value}&days=4&aqi=no&alerts=no`
 console.log(whatDay.value)
 
     fetch(url)
-    .then(async function(response) {
-        weather = await response.json()
-        let current = weather.forecast.forecastday.whatDay.hour.whatHour.condition
+    .then(response => response.json())
+    .then(data =>{
+        if(data.error){
+            alert('такої локації не знайденно')
+            return
+        }
+    
+        weather = data
+        
+        if(weather.error){
+            alert('локацію не знайдено')
+            return
+        }
+        let current = weather.forecast.forecastday[whatDay.value].hour[whatHour.value]
         let location = weather.location
         console.log(location.localtime_epoch)
         draw(current, location)
@@ -49,9 +66,15 @@ console.log(whatDay.value)
 
 
 function draw(current, location){
+    let index = weatherTypes.indexOf(current.condition.text)
+    if(index !== -1){
+        main.style.backgroundColor = weatherColor[index]
+        document.body.style.backgroundColor = weatherColor[index]
+    }
     information.style.visibility = "visible";
     information.innerHTML = (`
     <h1>${location.name}</h1>
+    <h2>${current.time}</h2>
     <div class="getInfo">
     <h2 class="getInfoBox" >Температура</h2>
     <h3 class="getInfoBox" id="temp">${current.temp_c}°C</h3>
@@ -66,8 +89,5 @@ function draw(current, location){
     <h3 class="getInfoBox" id="gustSpeed">Швидкість поривів ${current.gust_kph} км/год</h3>
     <h3 class="getInfoBox" id="windDir">Напрям вітру ${current.wind_dir}</h3>
     </div>
-    
-    <h4>Час останього оновлення ${current.last_updated}</h4>
-    
         `)
 }
